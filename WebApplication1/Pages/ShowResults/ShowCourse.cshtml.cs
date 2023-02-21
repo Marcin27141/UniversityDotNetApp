a34d086b92f036b207928e7911a4a68906624c13
Marcin <marcin.tkocz@op.pl>
2023-02-21 11:58:56 +0100

    Showing a list of students on ShowCourse if user HasAdminRights

--- WebApplication1/Pages/ShowResults/ShowCourse.cshtml.cs
+++ WebApplication1/Pages/ShowResults/ShowCourse.cshtml.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebApplication1.Services;
@@ -11,15 +13,24 @@ namespace WebApplication1.Pages.ShowResults
     public class ShowCourseModel : PageModel
     {
         private readonly IReadCourseOp _readCourseOp;
+        private readonly IAuthorizationService _authService;
+        public bool HasAdminRights { get; set; }
         public Course CourseToShow { get; set; }
 
-        public ShowCourseModel(IReadCourseOp readCourseOp) => _readCourseOp = readCourseOp;
+        public ShowCourseModel(IReadCourseOp readCourseOp, IAuthorizationService authService) {
+            _readCourseOp = readCourseOp;
+            _authService = authService;
+        } 
 
-        public IActionResult OnGet(string courseCode)
+        public async Task<IActionResult> OnGet(string courseCode)
         {
             CourseToShow = _readCourseOp.GetCourseByCode(courseCode);
             if (CourseToShow == null)
                 throw new Exception("Couldn't find the course");
+
+            var isAdmin = await _authService.AuthorizeAsync(User, "HasAdminRights");
+            HasAdminRights = isAdmin.Succeeded;
+
             return Page();
         }
     }
