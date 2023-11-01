e20a9aff1ba03070a32db0ed92a3db3774877375
Marcin <marcin.tkocz@op.pl>
2023-11-01 10:39:04 +0100

    added css and javascript to login page

---
+++ WebApplication1/wwwroot/js/login.js
@@ -0,0 +1,12 @@
+﻿const containerElement = document.getElementById('top-login-container');
+const forgotPasswordElement = document.getElementById('forgot-password-href');
+const backButton = document.getElementById('back-button');
+
+forgotPasswordElement.addEventListener('click', () => {
+    console.log("clicked");
+    containerElement.classList.add('active');
+})
+
+backButton.addEventListener('click', () => {
+    containerElement.classList.remove('active');
+})
