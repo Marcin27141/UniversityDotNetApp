0f082433ca41422430c0122c6e1207cdae9a1f02
Marcin27141 <marcin.tkocz@op.pl>
2023-11-04 14:13:48 +0100

    implemented partial views for showing entities

---
+++ WebApplication1/PartialModels/CoursePartialModel.cs
@@ -0,0 +1,10 @@
+﻿using WebApplication1.Services;
+
+namespace WebApplication1.PartialModels
+{
+    public class CoursePartialModel
+    {
+        public Course Course { get; set; }
+        public bool ShowStudents { get; set; }
+    }
+}
---
+++ WebApplication1/PartialModels/StudentPartialModel.cs
@@ -0,0 +1,10 @@
+﻿using WebApplication1.Services.People;
+
+namespace WebApplication1.PartialModels
+{
+    public class StudentPartialModel
+    {
+        public Student Student { get; set; }
+        public bool ShowCourses { get; set; }
+    }
+}
